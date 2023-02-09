a3459027690d895f866ae2ab25a2dfddaa24dbe2
tatys5394 <123910162+tatys5394@users.noreply.github.com>
2023-02-08 23:33:17 -0500

    confirmed password length and limited the least and most amount of characters

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+
+
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
+
 // Assignment code here
+var lowercaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbersChars = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var symbolsChar = ['!', '@', '#', '$', '%', '&', '*', '(', ')'];
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
@@ -10,8 +29,24 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+  // WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+
+  function generatePassword() {
+    var passwordLength = prompt("How long would you like your password to be?");
+
+    if (passwordLength, 8 || passwordLength > 128) {
+        alert ("Must be at least 8 characters and no more than 128 characters.");
+        return null;
+    }
+  }
+
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
